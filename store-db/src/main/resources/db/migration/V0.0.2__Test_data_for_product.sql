2dfc1385263da2b69a0786b8ae946e46ac0d549c
Daniel Incicau <daniel.incicau@busymachines.com>
2018-11-13 00:23:33 +0200

    Fixed product test data sql

--- store-db/src/main/resources/db/migration/V0.0.2__Test_data_for_product.sql
+++ store-db/src/main/resources/db/migration/V0.0.2__Test_data_for_product.sql
@@ -6,25 +6,25 @@ INSERT INTO product (c_category_id, name, price, discount, is_on_promotion, c_pr
 INSERT INTO product (c_category_id, name, price, discount, is_on_promotion, c_promotion_image, availability_on_command, description, care, added_at) VALUES (3, 'Valentina dress', 13.99, 0.9, false, NULL, true, ARRAY['Mesh body suit with dropped arm holes', '84% Nylon / 16% Spandex'], ARRAY['Hand rinse, dry in shade only', 'Avoid rough surfaces, lotions, sunscreens, and overly chlorinated pools', 'Suits should be worn with care'], NOW());
 INSERT INTO product (c_category_id, name, price, discount, is_on_promotion, c_promotion_image, availability_on_command, description, care, added_at) VALUES (3, 'Cassia dress', 14.99, 20.0, false, NULL, true, ARRAY['Mesh body suit with dropped arm holes', '84% Nylon / 16% Spandex'], ARRAY['Hand rinse, dry in shade only', 'Avoid rough surfaces, lotions, sunscreens, and overly chlorinated pools', 'Suits should be worn with care'], NOW());
 
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/1/DSC_3689.jpg', 'La vie en rose dress 1', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/1/DSC_3707.jpg', 'La vie en rose dress 2', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/1/DSC_3761.jpg', 'La vie en rose dress 3', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/2/DSC_3314.jpg', 'Valentina dress 1', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/2/DSC_3377.jpg', 'Valentina dress 2', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/2/DSC_3380.jpg', 'Valentina dress 3', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/3/DSC_4638.jpg', 'Cassia dress 1', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/3/DSC_4649.jpg', 'Cassia dress 2', 'jpg');
-INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/products/3/DSC_4708.jpg', 'Cassia dress 3', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/1/DSC_3689.jpg', 'La vie en rose dress 1', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/1/DSC_3707.jpg', 'La vie en rose dress 2', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/1/DSC_3761.jpg', 'La vie en rose dress 3', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/2/DSC_3314.jpg', 'Valentina dress 1', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/2/DSC_3377.jpg', 'Valentina dress 2', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/2/DSC_3380.jpg', 'Valentina dress 3', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/3/DSC_4638.jpg', 'Cassia dress 1', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/3/DSC_4649.jpg', 'Cassia dress 2', 'jpg');
+INSERT INTO content (content_id, name, format) VALUES ('clothes.store.app/product/3/DSC_4708.jpg', 'Cassia dress 3', 'jpg');
 
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (1, 'clothes.store.app/products/1/DSC_3689.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (1, 'clothes.store.app/products/1/DSC_3707.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (1, 'clothes.store.app/products/1/DSC_3761.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (2, 'clothes.store.app/products/2/DSC_3314.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (2, 'clothes.store.app/products/2/DSC_3377.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (2, 'clothes.store.app/products/2/DSC_3380.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (3, 'clothes.store.app/products/3/DSC_4638.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (3, 'clothes.store.app/products/3/DSC_4649.jpg');
-INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (3, 'clothes.store.app/products/3/DSC_4708.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (1, 'clothes.store.app/product/1/DSC_3689.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (1, 'clothes.store.app/product/1/DSC_3707.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (1, 'clothes.store.app/product/1/DSC_3761.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (2, 'clothes.store.app/product/2/DSC_3314.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (2, 'clothes.store.app/product/2/DSC_3377.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (2, 'clothes.store.app/product/2/DSC_3380.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (3, 'clothes.store.app/product/3/DSC_4638.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (3, 'clothes.store.app/product/3/DSC_4649.jpg');
+INSERT INTO product_content_map (p_product_id, c_content_id) VALUES (3, 'clothes.store.app/product/3/DSC_4708.jpg');
 
 INSERT INTO stock (p_product_id, product_size, available_count) VALUES (1, 'M', 23);
 INSERT INTO stock (p_product_id, product_size, available_count) VALUES (1, 'S', 18);
